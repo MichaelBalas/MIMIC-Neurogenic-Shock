64891c9cbfc3a2dcdc88836064fd992f38a06a5f
Michael Balas <mbalas@Monaco>
2018-06-05 08:15:35 -0400

    Include TBI & Severity for Analysis

--- Master_Tables/build_master.py
+++ Master_Tables/build_master.py
@@ -5,29 +5,38 @@ df2 = pd.read_csv("../Dead/DEAD.csv")
 df3 = pd.read_csv("../Spinal_Level_Mult/SPINAL_LEVEL_MULT.csv")
 df4 = pd.read_csv("../In_Shock/IN_SHOCK.csv")
 df5 = pd.read_csv("../EC_ES_Injury/EC_ES_INJURY.csv")
-df6 = pd.read_csv("../Sum_Comorbidities/SUM_COMORBIDITIES.csv")
-df7 = pd.read_csv("../SOFA/SOFA.csv")
+df6 = pd.read_csv("../TBI/TBI.csv")
+df7 = pd.read_csv("../Sum_Comorbidities/SUM_COMORBIDITIES.csv")
+df8 = pd.read_csv("../SOFA/SOFA.csv")
 
-
-dfs = [df1, df2, df3, df4, df5, df6, df7]
+dfs = [df1, df2, df3, df4, df5, df6, df7, df8]
 
 df = reduce(lambda left,right: pd.merge(left,right,on=["HADM_ID","ICUSTAY_ID"]), dfs)
 df.drop(columns=["ICD9_CODE","HEARTRATE_MEAN","SYSBP_MEAN"], inplace=True)
 df["IN_SHOCK"] = df["IN_SHOCK"].fillna("NA")
+df["GCS"] = df["GCS"].fillna("NA")
 df.drop(df[(df.AGE > 89)].index, inplace=True)
 
 ### Process the data for linear regression
+# Remove all NAs
+df.drop(df[df.IN_SHOCK == "NA"].index, inplace=True)
+df.drop(df[df.GCS == "NA"].index, inplace=True)
 # FEMALE = 0; MALE = 1
 df.GENDER = df.GENDER.eq('M').mul(1)
 # NO = 0; YES = 1
 df.DEAD = df.DEAD.eq('Y').mul(1)
 # NO = 0; YES = 1
-df.drop(df[df.IN_SHOCK == "NA"].index, inplace=True) # Remove all NAs
 df.IN_SHOCK = df.IN_SHOCK.eq('Y').mul(1)
 # NO = 0; YES = 1
 df.MULTIPLE_SCI = df.MULTIPLE_SCI.eq(True).mul(1)
 # NO = 0; YES = 1
 df.EC_ES_INJURY = df.EC_ES_INJURY.eq('Y').mul(1)
+df.replace("None", 1, inplace=True)
+df.replace("Mild", 2, inplace=True)
+df.replace("Moderate", 3, inplace=True)
+df.replace("Severe", 4, inplace=True)
+df.replace("Vegetative", 4, inplace=True)
+
 df = df.drop_duplicates()
 
 # C1C4, C5C7, C1C7 & MULTCERV = C; T1T6, T7T12 & T1T12 = T; L = L
@@ -55,7 +64,7 @@ df.loc[((df.congestive_heart_failure == 1) | (df.cardiac_arrhythmias == 1) | (df
 df.loc[((df.liver_disease == 1) | (df.coagulopathy == 1)), 'CMB_LIVER/COAG'] = 1
 
 # Pulmonary: (Chronic pulmonary)
-df.loc[(df.chronic_pulmonary), 'CMB_PULMONARY'] = 1
+df.loc[(df.chronic_pulmonary == 1), 'CMB_PULMONARY'] = 1
 
 # Diabetes: (Diabetes uncomplicated, Diabetes complicated)
 df.loc[((df.diabetes_uncomplicated == 1) | (df.diabetes_complicated == 1)), 'CMB_DIABETES'] = 1
@@ -72,8 +81,8 @@ df.loc[(df.alcohol_abuse == 1), 'CMB_ALCOHOL_ABUSE'] = 1
 # MENTAL HEALTH (Psychoses, Depression)
 df.loc[((df.psychoses == 1) | (df.depression == 1)), 'CMB_MENTAL_HEALTH'] = 1
 
-# Drop comorbidity columns ([16-46})
-df.drop(df.columns[16:46], axis=1, inplace=True)
+# Drop comorbidity columns ([17-47})
+df.drop(df.columns[17:47], axis=1, inplace=True)
 
 df.fillna(0, inplace=True)
 df.to_csv("MASTER_TABLE.csv", index=False)
---
+++ TBI/get_tbi.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+shock_ids = pd.read_csv("../Shock_IDs/SHOCK_IDS.csv")
+oasis = pd.read_csv("../Backbone/OASIS.csv")
+tbi = pd.merge(shock_ids, oasis, on=["HADM_ID","ICUSTAY_ID"], how='left')
+
+# Remove all unnecessary columns
+tbi = tbi[["HADM_ID","ICUSTAY_ID", "GCS"]]
+
+# If 1, convert to yes, if 2, convert to no
+
+tbi.loc[tbi['GCS'] > 15, 'GCS'] = "None"
+tbi.loc[((tbi['GCS'] >= 13) & (tbi['GCS'] <= 15)), 'GCS'] = "Mild"
+tbi.loc[((tbi['GCS'] >= 9) & (tbi['GCS'] <= 12)), 'GCS'] = "Moderate"
+tbi.loc[((tbi['GCS'] >= 3) & (tbi['GCS'] <= 8)), 'GCS'] = "Severe"
+tbi.loc[tbi['GCS'] < 3, 'GCS'] = "Vegetative"
+
+
+tbi.to_csv("TBI.csv", index=False)
+
+
